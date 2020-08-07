c0bc41813d442da7a2192e45400006e40cca08fc
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 12:45:13 -0600

    fixing bug

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace ifStatements
     {
         static void Main(string[] args)
         {
+            bool isMale = false;
+            bool isTall = false;
+
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male.");
+            }
+            else if (isMale && !isTall) {
+
+                Console.WriteLine("You are a short male");
+
+
+            }
+            else if (!isMale && isTall) {
+
+                Console.WriteLine("You are not male but are Tall");
+            
+            } else
+            {
+                Console.WriteLine("You are not male and not tall.");
+            }
+
             Console.ReadLine();
         }
     }
